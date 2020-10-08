1df4e92185c0eb9dc2d85cd1a5a88e8ea8adae98
mustak <mhyaqub@yahoo.com>
2020-10-08 22:02:14 +0100

    Add [157] http package to get data

--- lib/route/loading_screen.dart
+++ lib/route/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:clima_app/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -24,8 +25,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
     });
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+      // throw Exception('Failed to load weather data');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
         child: Column(
